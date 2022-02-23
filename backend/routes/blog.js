921872a11d641e7af336015299122ecef7773921
ThinThinzarHtet <56788855+ThinThinzarHtet@users.noreply.github.com>
2022-02-23 23:20:51 +0630

    node API setup and add signup validator

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { time } = require('../controllers/blog');
+router.get('/', time);
 
-router.get('/', (req, res) => {
-    res.json({ time: Date().toString() });
-})
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// bring routes
+const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
+
 // app
 const app = express();
 
@@ -27,10 +31,9 @@ if (process.env.NODE_ENV === 'development') {
     }));
 }
 
-// routes
-app.get('/api', (req, res) => {
-    res.json({ time: Date().toString() })
-});
+// routes middleware
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
