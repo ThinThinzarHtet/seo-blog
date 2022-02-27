d1ff65a1a25f06deda441630f0fadfbfe574a16d
ThinThinzarHtet <56788855+ThinThinzarHtet@users.noreply.github.com>
2022-02-27 12:18:09 +0630

    protecting routes for admin and user

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,11 +1,14 @@
 import Layout from '../../components/Layout';
+import Admin from '../../components/auth/Admin';
 import Link from 'next/link';
 
 const AdminIndex = () => {
   return (
     <>
       <Layout>
-        <h2>Admin Dashboard</h2>
+        <Admin>
+          <h2>Admin Dashboard</h2>
+        </Admin>
       </Layout>
     </>
   );
